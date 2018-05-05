d6488e2178f2b8c4a74654eba67d34a950042631
Hayden Crain <haydencrain@optusnet.com.au>
2018-05-05 17:27:36 +1000

    updates

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -2,7 +2,19 @@
 #include <vector>
 #include "weighted_graph.hpp"
 
-int main(){
+int main()
+{
 	weighted_graph<int> g;
-	std::cout << "not working";
+	int r = 5;
+	for (int i = 0; i < r; ++i)
+		g.add_vertex(i);
+	std::vector<int> vertices = g.get_vertices();
+	g.add_edge(0, 1, 1);
+	g.add_edge(1, 2, 2);
+	g.add_edge(2, 3, 3);
+	g.add_edge(3, 4, 4);
+	g.remove_vertex(0);
+	g.add_vertex(0);
+	g.add_edge(0, 4, 5);
+	g.print();
 }
--- Assignment1/weighted_graph.hpp
+++ Assignment1/weighted_graph.hpp
@@ -23,9 +23,12 @@ class weighted_graph
 {
 
 private:
-	// 2D vector which stores the weight of each edge.
-	std::vector< std::vector<int> > edges;
-	size_t size;
+	std::vector<std::vector<int> > adj_matrix;
+	std::vector<vertex> vertices;
+	int edges_count;
+
+	int get_index(const vertex &) const;
+	void reset_weights(const int &);
 
 	//The graph_iterator class provides an iterator
 	//over the vertices of the graph.
@@ -77,6 +80,8 @@ private:
 	};
 
 public:
+	void print() const;
+
 	weighted_graph();	//A constructor for weighted_graph. It should start empty.
 	~weighted_graph(); //A destructor. Depending on how you do things, this may
 										 //not be necessary.
@@ -96,7 +101,7 @@ public:
 																																		 //vertices to the new weight (the int).
 
 	int get_edge_weight(const vertex &, const vertex &) const; //Returns the weight on the edge between the two vertices.
-	int degree(const vertex &) const;													 //Returns the degree of the vertex.
+	int degree(const vertex &) const;													 //Returns the degree of the vertex. (e.g. the number of edges it has)
 	int weighted_degree(const vertex &);											 //Returns the sum of the weights on all the edges incident to the vertex.
 	int num_vertices() const;																	 //Returns the total number of vertices in the graph.
 	int num_edges() const;																		 //Returns the total number of edges in the graph (just the count, not the weight).
@@ -286,10 +291,37 @@ typename weighted_graph<vertex>::neighbour_iterator weighted_graph<vertex>::neig
 																WEIGHTED GRAPH
 *********************************************************************************************************************************************************************/
 
+// DELETE AFTER LOL
+template <typename vertex>
+void weighted_graph<vertex>::print() const
+{
+	std::cout << "    ";
+	for (auto v : vertices)
+	{
+		std::cout << v << "  ";
+	}
+	std::cout << "\n   ";
+	for (auto v : vertices)
+	{
+		std::cout << "---";
+	}
+	std::cout << "\n";
+	for (unsigned i = 0; i < num_vertices(); i++)
+	{
+		std::cout << vertices[i] << " | ";
+		for (unsigned j = 0; j < num_vertices(); j++)
+		{
+			std::cout << adj_matrix[i][j] << "  ";
+		}
+		std::cout << "\n";
+	}
+	std::cout << "\n";
+}
+
 template <typename vertex>
 weighted_graph<vertex>::weighted_graph()
 {
-	size = 0;
+	edges_count = 0;
 }
 
 template <typename vertex>
@@ -300,44 +332,125 @@ weighted_graph<vertex>::~weighted_graph()
 template <typename vertex>
 bool weighted_graph<vertex>::has_vertex(const vertex &u) const
 {
+	for (auto i : vertices)
+	{
+		if (i == u)
+			return true;
+	}
 	return false;
+}
+
+template <typename vertex>
+void weighted_graph<vertex>::reset_weights(const int &index)
+{
+	for (unsigned i = 0; i < num_vertices(); i++)
+	{
+		adj_matrix[index][i] = 0;
+		adj_matrix[i][index] = 0;
+	}
+}
+
+template <typename vertex>
+int weighted_graph<vertex>::get_index(const vertex &u) const
+{
+	for (unsigned i = 0; i < num_vertices(); i++)
+	{
+		if (vertices[i] == u)
+			return i;
+	}
+	return -1;
 }
 
 template <typename vertex>
 bool weighted_graph<vertex>::are_adjacent(const vertex &u, const vertex &v) const
 {
-	return false;
+	if (!(has_vertex(u) && has_vertex(v)))
+		return false;
+	return adj_matrix[get_index(u)][get_index(v)] > 0;
 }
 
 template <typename vertex>
 void weighted_graph<vertex>::add_vertex(const vertex &v)
 {
+	if (!has_vertex(v))
+	{
+		vertices.push_back(v);
+		// resize adj_matrix and set initial weights for new row/column
+		adj_matrix.resize(num_vertices());
+		for (unsigned i = 0; i < adj_matrix.size(); i++)
+		{
+			adj_matrix[i].resize(adj_matrix.size());
+		}
+		reset_weights(num_vertices() - 1);
+	}
 }
 
 template <typename vertex>
 void weighted_graph<vertex>::add_edge(const vertex &u, const vertex &v, const int &weight)
 {
+	if (has_vertex(u) && has_vertex(v))
+	{
+		int u_pos = get_index(u);
+		int v_pos = get_index(v);
+		// if there's already and edge, don't add an edge
+		if (adj_matrix[u_pos][v_pos] == 0 && u_pos != v_pos)
+		{
+			adj_matrix[u_pos][v_pos] = weight;
+			adj_matrix[v_pos][u_pos] = weight;
+			edges_count++;
+		}
+	}
 }
 
 template <typename vertex>
 void weighted_graph<vertex>::remove_vertex(const vertex &u)
 {
+	if (has_vertex(u))
+	{
+		int u_pos = get_index(u);
+		vertices.erase(vertices.begin() + u_pos);
+		adj_matrix.erase(adj_matrix.begin() + u_pos);
+		for (unsigned i = 0; i < adj_matrix.size(); i++)
+		{
+			adj_matrix[i].erase(adj_matrix[i].begin() + u_pos);
+		}
+	}
 }
 
 template <typename vertex>
 void weighted_graph<vertex>::remove_edge(const vertex &u, const vertex &v)
 {
+	adj_matrix[u][v] = 0;
+	adj_matrix[v][u] = 0;
+	edges_count--;
 }
 
 template <typename vertex>
 void weighted_graph<vertex>::set_edge_weight(const vertex &u, const vertex &v, const int &weight)
 {
+	if (has_vertex(u) && has_vertex(v))
+	{
+		int u_pos = get_index(u);
+		int v_pos = get_index(v);
+		// if there isn't an edge already, we can't set the edge weight because it doesn't exist
+		if (adj_matrix[u_pos][v_pos] > 0 && u_pos != v_pos)
+		{
+			adj_matrix[u_pos][v_pos] = weight;
+			adj_matrix[v_pos][u_pos] = weight;
+			edges_count++;
+		}
+	}
 }
 
 template <typename vertex>
 int weighted_graph<vertex>::get_edge_weight(const vertex &u, const vertex &v) const
 {
-	return 0;
+	if (has_vertex(u) && has_vertex(v))
+	{
+		return adj_matrix[get_index(u)][get_index(v)];
+	}
+	else
+		return -1; // if vertices don't exist, return error flag
 }
 
 template <typename vertex>
@@ -355,13 +468,13 @@ int weighted_graph<vertex>::weighted_degree(const vertex &u)
 template <typename vertex>
 int weighted_graph<vertex>::num_vertices() const
 {
-	return 0;
+	return vertices.size();
 }
 
 template <typename vertex>
 int weighted_graph<vertex>::num_edges() const
 {
-	return 0;
+	return edges_count;
 }
 
 template <typename vertex>
@@ -373,7 +486,7 @@ int weighted_graph<vertex>::total_weight()
 template <typename vertex>
 std::vector<vertex> weighted_graph<vertex>::get_vertices()
 {
-	return std::vector<vertex>();
+	return vertices;
 }
 
 template <typename vertex>
