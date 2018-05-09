8df2b23ae5796d2171d4294d11979b0b6771f199
Hayden Crain <haydencrain@optusnet.com.au>
2018-05-09 20:46:51 +1000

    Update spanning_tree_algorithms.cpp

--- Assignment1/spanning_tree_algorithms.cpp
+++ Assignment1/spanning_tree_algorithms.cpp
@@ -1,3 +1,53 @@
+struct edge {
+		int u_index, v_index, weight;
+	};
+	
+	void sort_edges(std::vector<std::pair<int, std::pair<int, int> > >&);
+	
+	class disjoint_set {
+	private:
+		std::unordered_map<int, int> parent;
+		
+	public:
+		void make_set(int); 
+		int find(int);
+		void make_union(int, int);
+	};
+
+template <typename vertex>	void weighted_graph<vertex>::disjoint_set::make_set(int size) {
+		for (int i = 0; i < size; i++) {
+				parent[i] = i;
+		}
+}
+
+template <typename vertex>	int weighted_graph<vertex>::disjoint_set::find(int k) {
+		if (parent[k] == k)
+				return k;
+		return find(parent[k]);
+}
+
+template <typename vertex>	void weighted_graph<vertex>::disjoint_set::make_union(int u, int v) {
+		int x = find(u);
+		int y = find(v);
+		parent[x] = y;
+}
+
+template <typename vertex> void weighted_graph<vertex>::sort_edges(std::vector<std::pair<int, std::pair<int, int> >& edges) {
+		int max_index;    
+		for(unsigned int i = 0; i<edges.size()-1;i++){
+   			max_index = i;
+				for(unsigned int j = i+1; j<edges.size();j++)
+						if(edges[j].weight > edges[max_index].weight)
+								max_index = j;
+        // swap values
+        if( max_index != i ){
+					edge temp = edges[i];
+					edges[i] = edges[max_index];
+					edges[max_index] = temp;
+				}
+    }    
+}
+
 template <typename vertex>	
 weighted_graph<vertex> kruskal() {
 	weighted_graph<vertex> mst_graph;
